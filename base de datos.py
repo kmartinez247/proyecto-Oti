5feeaa38fe22ec9d76caa846bb0dc58e33f0ff83
Rodriguez0007 <jsrlndrodrigues@gmail.com>
2026-05-07 19:51:29 -0600

    si

--- base de datos.py
+++ base de datos.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
 from openpyxl import Workbook, load_workbook
-import re
 import os
 
 nombre_archivo ='datos.xls'
@@ -13,73 +12,70 @@ else:
     #crear el libro de excel
     wb = Workbook ()
     ws = wb.active
-    ws.append(["Nombre", "edad", "email", "telefono", "dirección"])
+    ws.append(["Aparato", "Cantidad/peso", "Categoria", "Estado", "Algún comentario "])
 
 
 def guardar_datos():
-    nombre =entry_nombre.get()
-    edad =entry_edad.get()
-    email =entry_email.get()
-    telefono =entry_telefono.get()
-    direccion =entry_direcccion.get()
+    aparato =entry_aparato.get()
+    cantidad_peso =entry_cantidad_peso.get()
+    categoria =entry_categoria.get()
+    estado =entry_estado.get()
+    comentario =entry_comentario.get()
 
-    if not nombre or not edad or not email or not telefono or not direccion: 
+    if not aparato or not cantidad_peso or not categoria or not estado or not comentario: 
         messagebox.showwarning( "Advertencia", " Todos los campos son obligatorios ")
         return
     try:
-        edad = int(edad)
-        telefono = int(telefono)
+        cantidad_peso = int(cantidad_peso)
     except ValueError:
-        messagebox.showwarning( "Advertencia", " edad y telefono deben de ser numeros ")
+        messagebox.showwarning( "Advertencia", " La cantidad o peso deben ser cantidades enteras")
         return
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        messagebox.showwarning( "Advertencia", "el correo no es valido  ")
-        return
+
     
-    ws.append([nombre, edad, email, telefono, direccion])
+    ws.append([aparato, cantidad_peso, categoria, estado, comentario])
     wb.save(nombre_archivo)
     messagebox.showwarning( "Información", " Datos guardados con exito ")
 
-    entry_nombre.delete (0, tk.END)
-    entry_edad.delete (0, tk.END)
-    entry_email.delete (0, tk.END)
-    entry_telefono.delete (0, tk.END)
-    entry_direcccion.delete (0, tk.END)
+    entry_aparato.delete (0, tk.END)
+    entry_cantidad_peso.delete (0, tk.END)
+    entry_categoria.delete (0, tk.END)
+    entry_estado.delete (0, tk.END)
+    entry_comentario.delete (0, tk.END)
     
       
 
 root = tk.Tk()
-root.title ("Formulario de entrada de datos")
+root.title ("Formulario")
 
 root.configure (bg='#003B71')
 label_style = {"bg": '#003B71' , "fg": "white" }
 entry_style = {"bg": '#39A935' , "fg": "white" }
 
 
-label_nombre = tk.Label(root, text="Nombre", **label_style)
-label_nombre.grid(row=0, column=0, padx=10, pady=5)
-entry_nombre = tk.Entry(root, **entry_style)
-entry_nombre.grid(row=0, column=1, padx=10, pady=5)
+label_aparato = tk.Label(root, text="Nombre", **label_style)
+label_aparato.grid(row=0, column=0, padx=10, pady=5)
+entry_aparato = tk.Entry(root, **entry_style)
+entry_aparato.grid(row=0, column=1, padx=10, pady=5)
 
-label_edad = tk.Label(root, text="edad", **label_style)
-label_edad.grid(row=1, column=0, padx=10, pady=5)
-entry_edad = tk.Entry(root, **entry_style)
-entry_edad.grid(row=1, column=1, padx=10, pady=5)
+label_cantidad_peso = tk.Label(root, text="edad", **label_style)
+label_cantidad_peso.grid(row=1, column=0, padx=10, pady=5)
+entry_cantidad_peso = tk.Entry(root, **entry_style)
+entry_cantidad_peso.grid(row=1, column=1, padx=10, pady=5)
 
-label_email = tk.Label(root, text="email", **label_style)
-label_email.grid(row=2, column=0, padx=10, pady=5)
-entry_email = tk.Entry(root, **entry_style)
-entry_email.grid(row=2, column=1, padx=10, pady=5)
+label_categoria = tk.Label(root, text="email", **label_style)
+label_categoria.grid(row=2, column=0, padx=10, pady=5)
+entry_categoria = tk.Entry(root, **entry_style)
+entry_categoria.grid(row=2, column=1, padx=10, pady=5)
 
-label_telefono = tk.Label(root, text="Telefono", **label_style)
-label_telefono.grid(row=3, column=0, padx=10, pady=5)
-entry_telefono = tk.Entry(root, **entry_style)
-entry_telefono.grid(row=3, column=1, padx=10, pady=5)
+label_estado = tk.Label(root, text="Telefono", **label_style)
+label_estado.grid(row=3, column=0, padx=10, pady=5)
+entry_estado = tk.Entry(root, **entry_style)
+entry_estado.grid(row=3, column=1, padx=10, pady=5)
 
-label_direccion = tk.Label(root, text="Direccion", **label_style)
-label_direccion.grid(row=4, column=0, padx=10, pady=5)
-entry_direcccion = tk.Entry(root, **entry_style)
-entry_direcccion.grid(row=4,column=1, padx=10, pady=5)
+label_comentario = tk.Label(root, text="Direccion", **label_style)
+label_comentario.grid(row=4, column=0, padx=10, pady=5)
+entry_comentario = tk.Entry(root, **entry_style)
+entry_comentario.grid(row=4,column=1, padx=10, pady=5)
 
 boton_guardar = tk.Button(root,text="Guardar", command=guardar_datos, bg='#BD1C2C', fg='white', width=20 )
 boton_guardar.grid(row=5,column=0, columnspan=2, padx=10, pady=5)
